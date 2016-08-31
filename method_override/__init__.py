c6c114162b4f2d5fbc2402902397a67949691cf2
Pete Browne <pete.browne@localmed.com>
2016-08-31 10:38:35 -0500

    Bump version: 0.1.0 → 0.2.0

--- method_override/__init__.py
+++ method_override/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'LocalMed, Inc.'
 __copyright__ = 'Copyright 2012-2016 Localmed, Inc.'
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __version_info__ = tuple(__version__.split('.'))
 __short_version__ = __version__
